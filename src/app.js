541fc6d23ba15977c789e3950317619e4c9280c6
Sahil <sahildonde7@gmail.com>
2022-01-06 17:14:14 +0530

    port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const ejs = require("ejs");
 const buildTable = require("./script");
 
